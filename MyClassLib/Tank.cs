f0b08bb74c0053051b1dd6e2420cda2028d8518c
vrait35 <vrait35@mail.ru>
2018-09-19 02:39:09 +0600

    stavki_na_parimatch

---
+++ MyClassLib/Tank.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    namespace WorldOfTanks
+    {
+        public class Tank
+        {
+            public int Reservation { get; set; }
+            public int Ammunition { get; set; }
+            public int Maneuverability { get; set; }
+            public string Name{ get;set;}
+            public Tank() {
+                Reservation = 0;
+                Ammunition = 0;
+                Maneuverability = 0;
+            }
+            public Tank(int reservation,int ammunition,int maneuverability,string name)
+            {
+                if (reservation > 100 || ammunition > 100 || maneuverability > 100)
+                {
+                    throw new Exception("Превышает 100%");
+                }
+                else if (reservation < 0 || ammunition < 0 || maneuverability < 0)
+                {
+                    throw new Exception("Меньше 0%");
+                }
+                else if (name.Length == 0) throw new Exception("Название танка пустой ");
+                Reservation = reservation;
+                Ammunition = ammunition;
+                Maneuverability = maneuverability;
+                Name = name;
+            }
+            public static int  operator ^ (Tank t1,Tank t2)
+            {
+                int sum1 = 0, sum2 = 0;
+                if (t1.Reservation > t2.Reservation) sum1++;
+                else if (t1.Reservation < t2.Reservation) sum2++;
+                if (t1.Ammunition > t2.Ammunition) sum1++;
+                else if (t1.Ammunition < t2.Ammunition) sum2++;
+                if (t1.Maneuverability > t2.Maneuverability) sum1++;
+                else if (t1.Maneuverability < t2.Maneuverability) sum2++;
+                if (sum1 > sum2) return 1;
+                else if (sum1 < sum2) return 2;
+                return 0;
+            }
+            public static bool IsNumber(string numberAsString, ref int number)
+            {
+                try
+                {
+                    number = int.Parse(numberAsString);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Tanks/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib;
+using MyClassLib.WorldOfTanks;
+namespace Tanks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("              Добро пожаловать в Parimatch -  " +
+                "играют они выигрываете вы");
+            const int countWar = 5;
+            const int countPropertyTank = 3;
+            string Tank1Name = "T-34";
+            string Tank2Name = "Pantera";
+            int NumberTank = 0;
+            bool isNum = false;
+            int money = 0;
+            int sum1 = 0, sum2 = 0;
+            Tank[] tank1 = new Tank[countWar];
+            Tank[] tank2 = new Tank[countWar];
+            while (!isNum)
+            {
+            Console.WriteLine("Напишите номер танка за которого будете болеть  " +
+                " №1 = "+Tank1Name+"  №2 = "+Tank2Name+" № 0 = "+"Ничья(шанс маленький)");
+                isNum=Tank.IsNumber(Console.ReadLine(),ref NumberTank);
+                if (NumberTank>2 || NumberTank<0) isNum = false;
+            }
+            isNum = false;
+            while (!isNum)
+            {
+            Console.WriteLine("Введите сумму ставки ");
+                isNum = Tank.IsNumber(Console.ReadLine(), ref money);
+                if (money < 1) isNum = false;
+            }
+            isNum = false;
+
+            Random[] rnd = new Random[countPropertyTank];
+            int[] arrayRand = new int[countPropertyTank];
+            for (int i = 0; i < countPropertyTank; i++)
+                rnd[i] = new Random();
+            Console.WriteLine("Чтобы продолжить битву нажмите любую кнопку");
+
+            for (int i = 0; i < countWar; i++)
+            {
+                for (int j = 0; j < countPropertyTank; j++)
+                    arrayRand[j]= rnd[j].Next(0, 100);
+                try
+                {
+                    tank1[i] = new Tank(arrayRand[0], arrayRand[1], arrayRand[2], Tank1Name);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    i--;
+                    continue;
+                }
+                for (int j = 0; j < countPropertyTank; j++)
+                    arrayRand[j] = rnd[j].Next(0, 100);
+                try
+                {
+                    tank2[i] = new Tank(arrayRand[0], arrayRand[1], arrayRand[2], Tank2Name);
+                }
+                catch(Exception e) {
+                    Console.WriteLine(e.Message);
+                    i--;
+                    continue;
+                }
+                Console.Write("Поединок № " + (i+1)+" :  ");
+                if ((tank1[i] ^ tank2[i]) == 1)
+                {
+                    Console.WriteLine(tank1[i].Name + " победил ");
+                    sum1++;
+                }
+                else if ((tank1[i] ^ tank2[i]) == 2)
+                {
+                    Console.WriteLine(tank2[i].Name + " победил ");
+                    sum2++;
+                }
+                else if ((tank1[i] ^ tank2[i]) == 0)
+                {
+                    Console.WriteLine("Дружба победилa!");
+                }
+                Console.ReadKey();
+            }
+            if((NumberTank==1 && (sum1 > sum2)) || (NumberTank==2) && (sum2>sum1)
+                ||(NumberTank==0 && sum1==sum2))
+            {
+                Console.WriteLine("Вы выйграли "+(2*money)+"$");
+            }
+            else
+            {
+                Console.WriteLine("Вы проиграли,в след. раз получится ");
+            }
+        }
+    }
+}
